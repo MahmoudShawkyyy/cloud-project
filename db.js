1601335afdbca87a3c66b3570e84c14ab05f99f4
MahmoudShawkyyy <shawkymahmoud204@gmail.com>
2025-11-11 01:56:56 +0200

    Create backend database configuration file db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+// Database configuration file
+
+const config = {
+    host: 'localhost',
+    port: 5432,
+    user: 'your_username',
+    password: 'your_password',
+    database: 'your_database'
+};
+
+module.exports = config;
